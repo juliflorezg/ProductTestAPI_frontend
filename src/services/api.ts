436f99c090bd04ad0e96cdfb376970fa45be1956
Julian Florez <forezjuli08@hotmail.com>
2025-10-15 23:10:13 -0500

    feat(Products page): add url for API deployed

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,10 @@
 import axios from "axios";
 
+const API_URL = import.meta.env.PROD
+  ? "https://dotnetproductsapi1234.azurewebsites.net/api/products"
+  : "http://localhost:5297/api/products";;
+
 export const api = axios.create({
-  baseURL: 'http://localhost:5297/api/products',
+  baseURL: API_URL
+  ,
 });
